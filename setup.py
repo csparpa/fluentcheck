48f53f18e7fcca00eb1cd874998da12255df094b
csparpa <csparpa@gmail.com>
2017-06-12 17:53:22 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='check',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='tests',
+    license='MIT License',
+    description='Fluent assertions for Python values',
+    url='https://github.com/csparpa/check',
+    author='Claudio Sparpaglione',
+    author_email='csparpa@gmail.com',
+    classifiers=[
+      "License :: OSI Approved :: MIT License",
+      "Programming Language :: Python :: 2 :: Only",
+      "Natural Language :: English",
+      "Operating System :: OS Independent",
+      "Development Status :: 3 - Alpha",
+      "Intended Audience :: Developers",
+      "Topic :: Software Development :: Libraries"],
+)
