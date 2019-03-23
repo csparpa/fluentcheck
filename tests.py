dfd721ccfe97c473c9b1d4c924d8f4df34668dc8
Ben Dowen <33220797+dowenb@users.noreply.github.com>
2019-03-23 01:43:23 +0000

    fix tests to match re-worked uuid checks

--- tests.py
+++ tests.py
@@ -357,13 +357,14 @@ class TestCheck(unittest.TestCase):
         self.assertIsInstance(res, Check)
         try:
             Check('fluent').is_not_uuid1()
-        except:
             self.fail()
-    """ try:
+        except:
+            pass
+        try:
             Check('9194da68-31e8-11e9-8677-d8cb8a1a56c7').is_not_uuid1() # valid version 1 uuid
             self.fail()
         except CheckError:
-            pass"""
+            pass
 
     def test_is_uuid4(self):
         res = Check('80d2fde3-dc11-4e0c-a23d-3ebbfb203681').is_uuid4()
@@ -382,10 +383,13 @@ class TestCheck(unittest.TestCase):
     def test_is_not_uuid4(self):
         res = Check('f2b7a812-31e8-11e9-898b-d8cb8a1a56c7').is_not_uuid4() # version 1 uuid
         self.assertIsInstance(res, Check)
-    """    res = Check('fluent').is_not_uuid4()
-        self.assertIsInstance(res, Check)
+        try:
+            Check('fluent').is_not_uuid4()
+            self.fail()
+        except:
+            pass
         try:
             Check('725655d0-9840-4aec-9e3e-83e707dfa37c').is_not_uuid4() # version 4 uuid
             self.fail()
         except CheckError:
-            pass"""
+            pass
