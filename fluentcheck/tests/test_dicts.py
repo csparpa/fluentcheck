58e5ad67f139b584b5e1c8fb79f7504667ce5326
Dean Agan <agandfr@gmail.com>
2020-02-16 14:09:19 +1100

    Created unit test for dicts

---
+++ fluentcheck/tests/test_dicts.py
@@ -0,0 +1,45 @@
+import unittest
+from fluentcheck.check import Check, CheckError
+
+
+class TestDictsAssertions(unittest.TestCase):
+
+    def test_is_dict(self):
+        res = Check(dict()).is_dict()
+        self.assertIsInstance(res, Check)
+        try:
+            Check(123).is_dict()
+            self.fail()
+        except CheckError:
+            pass
+
+    def test_is_not_dict(self):
+        res = Check(set()).is_not_dict()
+        self.assertIsInstance(res, Check)
+        try:
+            Check(dict()).is_not_dict()
+            self.fail()
+        except CheckError:
+            pass
+
+    def test_has_keys(self):
+        d = { 1: 'one', 2: 'two'}
+        res = Check(d).has_keys(1,2)
+        self.assertIsInstance(res, Check)
+        try:
+            Check(d).has_keys(3,4)
+            self.fail()
+        except CheckError:
+            pass
+
+    def test_has_not_keys(self):
+        d = { 1: 'one', 2: 'two'}
+        res = Check(d).has_not_keys(3,4)
+        self.assertIsInstance(res, Check)
+        try:
+            Check(d).has_not_keys(1,2)
+            self.fail()
+        except CheckError:
+            pass
+
+    
