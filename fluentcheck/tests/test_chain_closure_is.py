993f5fb3df417f91fbb5c5ce926b8223f48f29fd
Michael Kennedy <mikeckennedy@gmail.com>
2020-03-12 11:13:08 -0700

    minor cleanup on comment.

--- fluentcheck/tests/test_chain_closure_is.py
+++ fluentcheck/tests/test_chain_closure_is.py
@@ -8,7 +8,6 @@ class TestIsChainClosure(unittest.TestCase):
     def test_chain_closure_with(self):
         # avoid linter issues like PyStatementEffect
         n = .5
-        # Is(n).not_none.number.truthy
         Is(n).not_none.number.truthy()
 
     def test_chain_closure_without(self):
