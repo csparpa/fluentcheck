f73bdd6b503d634b3f68434063a2f13ccbec8b87
csparpa <csparpa@gmail.com>
2020-04-04 15:49:28 +0200

    dumping 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='fluentcheck',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
