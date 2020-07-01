0bb95b55195ae3e29327dca7bf7af483681f2e91
Hedy Li <hedyhyry@gmail.com>
2020-07-01 10:49:47 +0800

    WIP almost finish test_sequences.py for Check

--- fluentcheck/tests/tests_check/test_sequences.py
+++ fluentcheck/tests/tests_check/test_sequences.py
@@ -25,8 +25,64 @@ class TestSequencesAssertions(unittest.TestCase):
     def test_is_iterable(self):
         res = Check(range(6)).is_iterable()
         self.assertIsInstance(res, Check)
+        res2 = Check([1, 2, 3]).is_iterable()
+        self.assertIsInstance(res2, Check)
         try:
             Check(8).is_iterable()
             self.fail()
         except CheckError:
             pass
+
+    def test_is_not_iterable(self):
+        res = Check(1).is_not_iterable()
+        self.assertIsInstance(res, Check)
+        try:
+            Check([1, 2, 3]).is_not_iterable()
+            self.fail()
+        except CheckError:
+            pass
+
+    def test_is_couple(self):
+        res = Check([1, 2]).is_couple()
+        self.assertIsInstance(res, Check)
+        res2 = Check(('1', '2')).is_couple()
+        self.assertIsInstance(res2, Check)
+        try:
+            Check([1, 2, 3]).is_couple()
+            self.fail()
+        except CheckError:
+            pass
+
+        try:
+            Check(10).is_couple()
+            self.fail()
+        except CheckError:
+            pass
+
+    def test_is_triplet(self):
+        res = Check([1, 2, 3]).is_triplet()
+        self.assertIsInstance(res, Check)
+        res2 = Check({'a': 1, 'b': 2, 'c': 3}).is_triplet()
+        self.assertIsInstance(res, Check)
+        try:
+            Check([1, 2]).is_triplet()
+            self.fail()
+        except CheckError:
+            pass
+
+        try:
+            Check({'a': 1, 'b': 2, 'c': 3, 'd': 4}).is_triplet()
+            self.fail()
+        except CheckError:
+            pass
+
+    def test_is_nuple(self):
+        obj = 1, 2, 3, 4, 5
+        res = Check(obj).is_nuple(5)
+        self.assertIsInstance(res, Check)
+        try:
+            Check((1, 2)).is_nuple(3)
+            self.fail()
+        except CheckError:
+            pass
+
